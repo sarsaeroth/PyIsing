2bc295ee2b85c8409776d29068c52cce70daa484
Peter Clark <pgclark@iastate.edu>
2024-12-19 18:49:14 -0700

    Check-in for final project

--- simulation.py
+++ simulation.py
@@ -1,23 +1,81 @@
+# simulation.py
 # This script contains functions for simulation of
 # a properly converted Ising spin glass
+
+import numpy as np
+from qiskit.quantum_info import SparsePauliOp
+
+class MissingHamiltonianError(Exception):
+    """Raised when a required Hamiltonian is missing."""
+    pass
+
+class NotExecutedError(Exception):
+    """Raised when results are requested before execution."""
+    pass
 
 class Simulation:
 
     def __init__(self):
-        self.num_qubits = False
-        self.num_steps = 
-        self.target_hamiltonian = False
-        self.transverse_hamiltonian = False
-        self.spectral_gap = False
+        self.num_qubits = None
+        self.num_steps = None
+        self.target_hamiltonian = None
+        self.transverse_hamiltonian = None
+        self.spectral_gaps = []
+        self.simulation_results = None
 
     def initialize(self, spinglass, num_steps=100):
+        if spinglass.hamiltonian is None:
+            raise MissingHamiltonianError("SpinGlass object does not contain a valid Hamiltonian.")
+
         self.target_hamiltonian = spinglass.hamiltonian
-        self.num_qubits = self.target_hamiltonian.num_qubits
+        self.num_qubits = spinglass.hamiltonian.num_qubits
+        self.num_steps = num_steps
+
+        transverse_terms = []
+        for i in range(self.num_qubits):
+            x_term = 'I' * i + 'X' + 'I' * (self.num_qubits - i - 1)
+            transverse_terms.append((x_term, -1))
+        pauli_terms, coeffs = zip(*transverse_terms)
+        self.transverse_hamiltonian = SparsePauliOp.from_list(
+            list(zip(pauli_terms, coeffs))
+        )
 
     def execute(self):
-        if not self.target_hamiltonian or not self.transverse_hamiltonian:
-            raise MissingHamiltonianError
+        if self.target_hamiltonian is None or self.transverse_hamiltonian is None:
+            raise MissingHamiltonianError("Hamiltonians are not properly initialized.")
+
+        simulation_data = []
+        spectral_gaps = []
+
+        delta_t = 1 / self.num_steps
+        current_state = np.ones(2 ** self.num_qubits) / np.sqrt(2 ** self.num_qubits)
+
+        for t in range(self.num_steps):
+            s = t / (self.num_steps - 1)
+
+            hamiltonian_t = (1 - s) * self.transverse_hamiltonian + s * self.target_hamiltonian
+
+            evolution_operator = np.linalg.matrix_power(
+                np.eye(2 ** self.num_qubits) - 1j * delta_t * hamiltonian_t.to_matrix(), 1
+            )
+
+            current_state = evolution_operator @ current_state
+
+            eigenvalues = np.linalg.eigvalsh(hamiltonian_t.to_matrix())
+            sorted_eigenvalues = sorted(eigenvalues)
+            gap = min(np.diff(sorted_eigenvalues))
+            spectral_gaps.append(gap)
+
+            simulation_data.append(current_state)
+
+        self.spectral_gaps = spectral_gaps
+        self.simulation_results = simulation_data
 
     def results(self):
-        if not self.spectral_gap:
-            raise NotExecutedError
+        if self.simulation_results is None:
+            raise NotExecutedError("Simulation has not been executed yet.")
+        return {
+            "spectral_gaps": self.spectral_gaps,
+            "simulation_data": self.simulation_results,
+        }
+
--- spinglass.py
+++ spinglass.py
@@ -1,55 +1,145 @@
 # This script contains functions for converting
 # an NP-complete problem into its corresponding
 # Ising spin glass model
+import networkx as nx
 from qiskit.quantum_info import SparsePauliOp
 
+class InvalidGraphError(Exception):
+    # Raised when the input graph is invalid
+    print(Exception)
+    pass
+
+class InvalidSetError(Exception):
+    # Raised when the input set is invalid
+    print(Exception)
+    pass
+
+class InvalidProblemError(Exception):
+    # Raised when the specified problem is not supported
+    print(Exception)
+    pass
+
 class SpinGlass:
-    
+
     def __init__(self):
-        self.hamiltonian = False
+        self.hamiltonian = None
 
     def fromGraph(self, G, problem):
-        #Input validation
-        if not type(G) is not networkx.classes.graph.Graph:
-            #Only supports Networkx for now
-            return InvalidGraphError
-        problems = ['hamiltonian']
-        if problem not in problems:
-            raise InvalidProblemError
+        if not isinstance(G, nx.Graph):
+            raise InvalidGraphError("Input is not a valid NetworkX graph.")
 
-        if problem == 'hamiltonian':
-            #Finding a Hamiltonian path in a graph
-            pass
-    
+        problems = ['clique', 'coloring', 'covering', 'fes', 'hamiltonian', 'partition']
+        if problem not in problems:
+            raise InvalidProblemError("Problem not supported.")
+
+        hamiltonian_terms = []
+        n = G.number_of_nodes()
+
+        if problem == 'clique':
+            for i in range(n):
+                z_term = 'I' * i + 'Z' + 'I' * (n - i - 1)
+                hamiltonian_terms.append((z_term, -1))
+
+            for i in range(n):
+                for j in range(i + 1, n):
+                    if not G.has_edge(i, j):
+                        z_term = 'I' * i + 'Z' + 'I' * (j - i - 1) + 'Z' + 'I' * (n - j - 1)
+                        hamiltonian_terms.append((z_term, 1))
+
+        elif problem == 'coloring':
+            for u, v in G.edges():
+                for color in range(4):  # Four-color theorem
+                    z_term_u = 'I' * u + 'Z' + 'I' * (n - u - 1)
+                    z_term_v = 'I' * v + 'Z' + 'I' * (n - v - 1)
+                    combined_term = ''.join(['I' if i != u and i != v else 'Z' for i in range(n)])
+                    hamiltonian_terms.append((combined_term, 1))
+            for u in G.nodes():
+                z_term = 'I' * u + 'Z' + 'I' * (n - u - 1)
+                hamiltonian_terms.append((z_term, -1))
+
+        elif problem == 'covering':
+            for u in G.nodes():
+                adjacent_sum_term = ''.join(['I' if i != u else 'Z' for i in range(n)])
+                hamiltonian_terms.append((adjacent_sum_term, 1))
+            for u in G.nodes():
+                z_term = 'I' * u + 'Z' + 'I' * (n - u - 1)
+                hamiltonian_terms.append((z_term, -1))
+
+        elif problem == 'fes':
+            for u, v in G.edges():
+                z_term = 'I' * u + 'Z' + 'I' * (v - u - 1) + 'Z' + 'I' * (n - v - 1)
+                hamiltonian_terms.append((z_term, 1))
+            for u, v in G.edges():
+                z_term = 'I' * u + 'Z' + 'I' * (v - u - 1) + 'Z' + 'I' * (n - v - 1)
+                hamiltonian_terms.append((z_term, -1))
+
+        elif problem == 'hamiltonian':
+            n = G.number_of_nodes()
+            for u in G.nodes():
+                for v in G.nodes():
+                    if u != v:
+                        z_term = 'I' * u + 'Z' + 'I' * (v - u - 1) + 'Z' + 'I' * (n - v - 1)
+                        hamiltonian_terms.append((z_term, -1))
+            for u in G.nodes():
+                z_term = 'I' * u + 'Z' + 'I' * (n - u - 1)
+                hamiltonian_terms.append((z_term, 1))
+            for u in G.nodes():
+                for v in G.nodes():
+                    if not G.has_edge(u, v):
+                        z_term = 'I' * u + 'Z' + 'I' * (v - u - 1) + 'Z' + 'I' * (n - v - 1)
+                        hamiltonian_terms.append((z_term, 2))
+
+        elif problem == 'partition':
+            for u in G.nodes():
+                z_term = 'I' * u + 'Z' + 'I' * (n - u - 1)
+                hamiltonian_terms.append((z_term, 1))
+            for u, v in G.edges():
+                z_term = 'I' * u + 'Z' + 'I' * (v - u - 1) + 'Z' + 'I' * (n - v - 1)
+                hamiltonian_terms.append((z_term, 2))
+
+        else:
+            raise InvalidProblemError(f"Problem {problem} is not yet implemented.")
+
+        # Assemble Hamiltonian
+        if hamiltonian_terms:
+            pauli_terms, coeffs = zip(*hamiltonian_terms)
+            self.hamiltonian = SparsePauliOp.from_list(
+                list(zip(pauli_terms, coeffs))
+            )
+
     def fromSet(self, S, problem):
-        #Input validation
-        if type(S) is list:
-            if (len(set(S)) != len(S)):
-                raise InvalidSetError
-        if type(S) is not set and type(S) is not list:
-            raise InvalidSetError
+        # Input validation
+        if isinstance(S, list):
+            if len(set(S)) != len(S):
+                raise InvalidSetError("Set contains duplicate elements.")
+        elif not isinstance(S, set):
+            raise InvalidSetError("Input is not a valid set or list.")
+
         problems = ['partition']
         if problem not in problems:
-            raise InvalidProblemError
+            raise InvalidProblemError("Problem not supported.")
+
         N = len(S)
-        
+        hamiltonian_terms = []
+
         if problem == 'partition':
-            #Dividing set into two disjoint subsets of equal sum 
-            hamiltonian_terms = []
+            # Dividing set into two disjoint subsets of equal sum
             for i in range(N):
-                #Self-interaction terms
+                # Self-interaction terms
                 coeff = S[i] ** 2
-                z_term = 'I'*i + 'Z' + 'I'*(N-i-1)
+                z_term = 'I' * i + 'Z' + 'I' * (N - i - 1)
                 hamiltonian_terms.append((z_term, coeff))
             for i in range(N):
-                for j in range(i+1,N):
-                    #Cross-interaction terms
-                    coeff = 2*S[i]*S[j]
-                    z_term = 'I'*i + 'Z' + 'I'*(j-i-1)
-                    z_term = z_term + 'Z' + 'I'*(N-j-1)
+                for j in range(i + 1, N):
+                    # Cross-interaction terms
+                    coeff = 2 * S[i] * S[j]
+                    z_term = 'I' * i + 'Z' + 'I' * (j - i - 1) + 'Z' + 'I' * (N - j - 1)
                     hamiltonian_terms.append((z_term, coeff))
+
+        # Assemble Hamiltonian
+        if hamiltonian_terms:
             pauli_terms, coeffs = zip(*hamiltonian_terms)
-            hamiltonian = SparsePauliOp.from_list(
-                    list(zip(pauli_terms, coeffs))
+            self.hamiltonian = SparsePauliOp.from_list(
+                list(zip(pauli_terms, coeffs))
             )
-            return hamiltonian
+
